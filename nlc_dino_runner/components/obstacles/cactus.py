e1fd47e7bc4aead596246f1a2039389b765a67fd
Rocio Encinas <chio.ref@gmail.com>
2021-09-14 20:19:46 -0400

    Clase 4 - Colisiones

---
+++ nlc_dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,12 @@
+
+# clase hija
+import random
+
+from nlc_dino_runner.components.obstacles.obstacles import Obstacles
+
+
+class Cactus(Obstacles):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 315
